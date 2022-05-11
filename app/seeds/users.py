4d4c20a488156e12a268cc0c7121dc5a1394a230
James Chen <jameschen09@yahoo.com>
2022-05-10 23:08:29 -0700

    added docker commands

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,16 +3,21 @@ from app.models import db, User
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+    demo = User(username='demo', firstName='Demo', lastName='User', email='demo@aa.io', password='password', bio='Thank you for trying out Nonogram! 🐶', profilePictureUrl='', privateStatus='False')
+    james = User(username='james', firstName='James', lastName='Chen', email='james@nonogram.com', password='password', bio='Literally the coolest person in the world. No cap. 😎', profilePictureUrl='https://res.cloudinary.com/jameschenn/image/upload/v1652238728/Nonogram/73676915_yzrlfq.jpg', privateStatus='False')
+    dayton = User(username='dayton', firstName='Dayton', lastName='Chen', email='dayton@nonogram.com', password='password', bio='Searching far and wide for a killer whale of my own... 🐋', profilePictureUrl='https://res.cloudinary.com/jameschenn/image/upload/v1652238731/Nonogram/94093737_ffe07t.jpg', privateStatus='False')
+    grant = User(username='grant', firstName='Grant', lastName='Walton', email='grant@nonogram.com', password='password', bio='Live, breath, code, and maybe some League of Legends in-between.', profilePictureUrl='https://res.cloudinary.com/jameschenn/image/upload/v1652239160/Nonogram/65691441_rztota.jpg', privateStatus='False')
+    huyen = User(username='huyen', firstName='Huyen', lastName='Nguyen', email='huyen@nonogram.com', password='password', bio='Is it nap time yet? 😴', profilePictureUrl='https://res.cloudinary.com/jameschenn/image/upload/v1652238737/Nonogram/92718001_dbhz5u.jpg', privateStatus='False')
+    khoi = User(username='khoi', firstName='Khoi', lastName='Duong', email='khoi@nonogram.com', password='password', bio='Invest in KHOIN. To the moon!! 🚀🚀🚀', profilePictureUrl='https://res.cloudinary.com/jameschenn/image/upload/v1652238735/Nonogram/92695890_h2lduf.jpg', privateStatus='False')
+    brad = User(username='brad', firstName='Brad', lastName='Simpson', email='brad@nonogram.com', password='password', bio='Only the greatest cohort lead of all time.', profilePictureUrl='https://res.cloudinary.com/jameschenn/image/upload/v1652238742/Nonogram/59807764_nnsnau.jpg', privateStatus='False')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(james)
+    db.session.add(dayton)
+    db.session.add(grant)
+    db.session.add(huyen)
+    db.session.add(khoi)
+    db.session.add(brad)
 
     db.session.commit()
 
