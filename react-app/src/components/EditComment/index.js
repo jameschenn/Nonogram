c962a70aec1e49818bd6ab84a97931e6642c8856
James Chen <jameschen09@yahoo.com>
2022-05-13 08:48:35 -0700

    bug checks

--- react-app/src/components/EditComment/index.js
+++ react-app/src/components/EditComment/index.js
@@ -3,3 +3,44 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import * as imageActions from "../../store/images";
 import * as commentActions from "../../store/comments";
+
+const EditCommentForm = () => {
+
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const sessionUser = useSelector(state => state.session.user)
+  const images = useSelector(state => state.images)
+  const comments = useSelector(state => state.comments)
+
+  useEffect(async () => {
+    await dispatch(imageActions.loadUserImagesThunk(id))
+  }, [dispatch])
+
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    const payload = {
+      comment,
+    }
+    dispatch(commentActions.editCommentThunk(id, payload))
+  }
+
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            value={comment}
+            onChange={(e => setComment(e.target.value))}
+            />
+        </div>
+        <button type="Submit">Edit Comment</button>
+      </form>
+    </section>
+  )
+}
