4f6e191b0545379b3cb82c0cdb810385abfddb30
James Chen <jameschen09@yahoo.com>
2022-05-14 17:58:40 -0700

    some css on the main feed

--- react-app/src/components/AllImages/index.js
+++ react-app/src/components/AllImages/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import * as imageActions from '../../store/images';
 import * as commentActions from '../../store/comments'
+import './AllImages.css'
 
 const AllImages = () => {
 
@@ -14,7 +15,7 @@ const AllImages = () => {
   const images = useSelector(state => state.images);
   const imageData = Object.values(images)
   const comments = useSelector(state => state.comments)
-  console.log('--------------------', comments)
+  console.log('IMAGE DATA', imageData)
 
   useEffect(async () => {
     await dispatch(imageActions.loadAllImagesThunk())
@@ -26,10 +27,13 @@ const AllImages = () => {
   return (
     <>
     {imageData.map((image, idx) => (
-      <ul>
-        <li><img src={image?.imageUrl} alt={image?.id}/></li>
-        <li>{image?.caption}</li>
-      </ul>
+      <div className='img-feed-card'>
+        <ul>
+          <li><a href={`/users/${image.userId}`}><img src={image?.user?.profilePictureUrl} alt='profile-icon' className='profile-icon' style={{ marginLeft: '25px' }} /></a> <span style={{fontWeight:'bold', marginLeft: '15px'}}>{image?.user?.username}</span></li>
+          <li><a href={`/images/${image.id}`}><img src={image?.imageUrl} alt={image?.id}/></a></li>
+          <li> <span style={{ fontWeight: 'bold', marginLeft:'25px', marginRight: '15px' }}>{image?.user?.username}</span> {image?.caption}</li>
+        </ul>
+      </div>
     ))}
     </>
   )
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,43 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import LogoutButton from './auth/LogoutButton';
+import './NavBar.css'
 
 const NavBar = () => {
+
+  const sessionUser = useSelector(state => state.session.user);
+
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+      <div className='nav-links'>
+        <ul>
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink>
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
