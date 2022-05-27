fefda94043a0d4d39bef271434b13ecdc6289dec
James Chen <jameschen09@yahoo.com>
2022-05-26 23:48:44 -0700

    fixed weird positioning of the modal

--- react-app/src/components/IndividualImage/index.js
+++ react-app/src/components/IndividualImage/index.js
@@ -65,86 +65,87 @@ const IndividualImage = () => {
 
   return (
     <>
-    {loading ? (
+      {loading ? (
 
         <div className="loading">
           <ClipLoader color={'#50E3C2'} loading={loading} size={150} />
         </div>
 
-    ) : (
+      ) : (
 
-      imageData ? (
-        <div className='post-container'>
-        <div className='post-image'>
-        <img src={imageData?.imageUrl} alt='post' />
-        </div>
-        <div className='post-info'>
-          <p><a href={`/users/${imageData?.userId}`}><img src={imageData?.user?.profilePictureUrl} alt='profile-icon' className='profile-icon' /> <span style={{ fontWeight: 'bold', marginRight: '10px' }}>{imageData?.user?.username}</span></a>
-              {user?.id === imageData?.userId && (
-                <>
-                  <Popup trigger={<i class="fa-solid fa-ellipsis"></i>} position="right center">
-                    {close => (
-                    <>
-                    <p>Edit Your Caption</p>
-                    <EditImageForm imageId={imageData?.id} close={close} />
-                    <p>Delete Your Image</p>
-                    <button type='button' onClick={() => {
-                      dispatch(imageActions.deleteImageThunk(imageData?.id)).then(() => history.push(`/me`))
-                    }}>Delete</button>
-                    </>
-                    )}
-                  </Popup>
-                </>
-              )}
-              </p>
-              <div className='individual-post-caption'>
-              <p style={{marginTop: '10px'}}>{imageData?.caption}</p>
-              </div>
-              <div className='individual-comments'>
-            {commentsData.map((comment, idx) => (
-              <ul>
-                <li> <a href={`/users/${comment?.userId}`}><img src={comment.user.profilePictureUrl} alt='profile-icon' className='profile-icon' /> <span style={{ fontWeight: 'bold' }}>{comment.user.username}</span></a> {comment.comment} {user?.id === comment?.userId && (
+        imageData ? (
+          <div className='post-container'>
+            <div className='post-image'>
+              <img src={imageData?.imageUrl} alt='post' />
+            </div>
+            <div className='post-info'>
+              <p><a href={`/users/${imageData?.userId}`}><img src={imageData?.user?.profilePictureUrl} alt='profile-icon' className='profile-icon' /> <span style={{ fontWeight: 'bold', marginRight: '10px' }}>{imageData?.user?.username}</span></a>
+                {user?.id === imageData?.userId && (
                   <>
                     <Popup trigger={<i class="fa-solid fa-ellipsis"></i>} position="right center">
                       {close => (
                         <>
-                      <p>Edit Your Comment</p>
-
-                      <EditCommentForm commentId={comment} close={close} />
-                      <button type='button' onClick={() => { dispatch(commentActions.deleteCommentThunk(comment.id)) }}> Delete Comment</button>
-                      </>
+                          <p>Edit Your Caption</p>
+                          <EditImageForm imageId={imageData?.id} close={close} />
+                          <p>Delete Your Image</p>
+                          <button type='button' onClick={() => {
+                            dispatch(imageActions.deleteImageThunk(imageData?.id)).then(() => history.push(`/me`))
+                          }}>Delete</button>
+                        </>
                       )}
-                      </Popup>
+                    </Popup>
                   </>
-                )}</li>
-              </ul>
-            ))}
-          </div>
-          <div className="individual-likes-and-comment">
-            {like ? (
-              <button onClick={handleLike}>❤️</button>
-              ) : (
-                <button onClick={handleLike}>🤍</button>
                 )}
+              </p>
+              <div className='individual-post-caption'>
+                <p style={{ marginTop: '10px' }}>{imageData?.caption}</p>
+              </div>
+              <div className='individual-comments'>
+                {commentsData.map((comment, idx) => (
+                  <ul>
+                    <li> <a href={`/users/${comment?.userId}`}><img src={comment.user.profilePictureUrl} alt='profile-icon' className='profile-icon' /> <span style={{ fontWeight: 'bold' }}>{comment.user.username}</span></a> {comment.comment} {user?.id === comment?.userId && (
+                      <>
+                        <Popup trigger={<i class="fa-solid fa-ellipsis"></i>} position="right center">
+                          {close => (
+                            <>
+                              <p>Edit Your Comment</p>
+
+                              <EditCommentForm commentId={comment} close={close} />
+                              <button type='button' onClick={() => { dispatch(commentActions.deleteCommentThunk(comment.id)) }}> Delete Comment</button>
+                            </>
+                          )}
+                        </Popup>
+                      </>
+                    )}</li>
+                  </ul>
+                ))}
+              </div>
+              <div className="individual-likes-and-comment">
+                {like ? (
+                  <button onClick={handleLike}>❤️</button>
+                ) : (
+                  <button onClick={handleLike}>🤍</button>
+                )}
+
                   <Popup trigger={<p>{likes?.length} likes</p>} position="left bottom">
                     {close => (
                       <>
-                        <p style={{ marginTop: '10px', padding: '5px', fontWeight: 'bold', borderBottom: 'solid 1px lightgray' }}>Likes</p>
+                        <p style={{ fontWeight: 'bold', borderBottom: 'solid 1px lightgray', marginRight: '50px' }}>Likes</p>
                         <LikesList />
                       </>
                     )}
                   </Popup>
-          <div className="individual-post-comment">
-          <PostComment />
+                <div className="individual-post-comment">
+                  <PostComment />
+                </div>
+              </div>
+            </div>
           </div>
-          </div>
-          </div>
-          </div>
-          ) :
+        ) :
           (
             <ErrorPage />
-            )
-        )}
+          )
+      )}
     </>
   )
 }
